42b5074cd87dd3af55882cd484b398d844170a9c
ambrose-kibet <ambrosekibet36@gmail.com>
2023-03-14 16:49:55 +0300

    add profile branch

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,2 +1,29 @@
-const Profile = () => <div>Profile</div>;
+import { useAppSelctor } from '../redux/reduxHooks';
+
+const Profile = () => {
+  const { missionList } = useAppSelctor((state) => state.missions);
+  const filteredList = missionList.filter(
+    (mission) => mission.reserved === true,
+  );
+  return (
+    <sectioon className="section-container two-column">
+      <h2 className="title-three"> My Rockects</h2>
+      <h2 className="title-three"> My missions</h2>
+      <ul className="display-items">No Rockects to display</ul>
+      <ul className="display-items">
+        {filteredList.length > 0 ? (
+          filteredList.map((mision) => (
+            <li key={mision.mision_id}>
+              <p>{mision.mission_name}</p>
+            </li>
+          ))
+        ) : (
+          <li>
+            <h3 className="title-three">No missions at the moment</h3>
+          </li>
+        )}
+      </ul>
+    </sectioon>
+  );
+};
 export default Profile;
