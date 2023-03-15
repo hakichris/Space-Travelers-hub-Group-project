e3cbf723c5b302a0386443503b9f5423574d5208
Chris <hakizachrist1@gmail.com>
2023-03-15 16:20:56 +0200

    Update SingleRocket

--- src/components/SingleRocket.jsx
+++ src/components/SingleRocket.jsx
@@ -1,29 +1,40 @@
-import { useDispatch } from 'react-redux';
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
 import { toggleRocket } from '../redux/rocketsSlice';
 
 const Rocket = ({
-  id, rocketName: name, description, flickrImages, reserved,
+  id, rocketName, description, Image, reserved,
 }) => {
   const dispatch = useDispatch();
-
   return (
     <div className="container">
-      <div className="image">{flickrImages}</div>
+      <div><img className="image" src={Image} alt=" " /></div>
       <div className="rocketDetails">
-        <h1>{name}</h1>
-        <p className="description">
-          <span className="reservation">
+        <h1 className="margin">{rocketName}</h1>
+        <p className="description margin">
+          <span className="reservation margin">
             {' '}
-            {reserved ? 'Reserved' : ' '}
+            {reserved ? 'Reserved' : ' ' }
             {' '}
           </span>
           {description}
         </p>
-        <button type="button" onClick={() => dispatch(toggleRocket(id))}>Reserve Rocket</button>
+        <button className={reserved ? 'reservedbutton' : 'reserverocket'} type="button" onClick={() => dispatch(toggleRocket(id))}>
+          {' '}
+          {reserved ? 'Cancel reservation' : 'Reserve Rocket' }
+        </button>
       </div>
     </div>
   );
+};
+
+Rocket.propTypes = {
+  id: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  rocketName: PropTypes.string.isRequired,
+  Image: PropTypes.string.isRequired,
+  reserved: PropTypes.bool.isRequired,
 };
 
 export default Rocket;
